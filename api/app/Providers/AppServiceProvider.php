c3c35be997da7916cf92ec4553ad65bb1cad1e3e
Sabino Machado <sabino_fjv@hotmail.com>
2025-04-21 19:40:19 -0300

    force https

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repositories\Crud\CrudRepository;
 use App\Repositories\Crud\EloquentCrudRepository;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!$this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
